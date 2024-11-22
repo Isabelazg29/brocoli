82e54bb330d9bed0eb13cbf63f1c9500b97467bb
Isabelazg29 <isabela.zg29@gmail.com>
2024-11-21 22:09:52 -0500

    primer commit

---
+++ Glamping_Addventure2/Models/Servicios/Contrato/IUsuarioService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Glamping_Addventure2.Models;
+
+namespace Glamping_Addventure.Models.Servicios.Contrato
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> GetUsuario(string email, string contrasena);
+
+        Task<Usuario> SaveUsuario(Usuario modelo);
+
+        Task<Usuario> GetUsuarioPorEmail(string email);
+
+        Task<Usuario> GetUsuarioPorDocumento(int? numeroDocumento);
+
+        Task<Role> GetRolPorId(int Idrol);
+
+    }
+}
